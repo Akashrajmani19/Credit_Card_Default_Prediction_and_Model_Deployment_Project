2fa240caa8b89adf003efafa1fa5d20b84e84528
Akash vaishnav <aakashrankawat19@gmail.com>
2024-01-28 13:42:34 +0530

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Credit_Card_Default_Prediction',
+    version = '0.0.1',
+    author = 'Akash Vaishnav',
+    author_email = 'akash.vaishnav1908@gmail.com',
+    packages = find_packages()
+)
---
+++ src/Credit_Card_Default_Prediction/components/data_integration.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+class DataIngestion:
+    def __init__(self,):
+        pass
+
+    def initiate_data_ingestion(self):
+        pass
