2e47ccbe3e839e43c2255956d8cc0df800a4d822
Akash vaishnav <aakashrankawat19@gmail.com>
2024-01-28 15:47:19 +0530

    exception module is added

--- src/Credit_Card_Default_Prediction/logger.py
+++ src/Credit_Card_Default_Prediction/logger.py
@@ -16,3 +16,6 @@ logging.basicConfig(level = logging.INFO,
                     filename = LOG_FILE_PATH,
                     format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
                     )
+
+if __name__ =="__main__":
+    logging.info('testing')
