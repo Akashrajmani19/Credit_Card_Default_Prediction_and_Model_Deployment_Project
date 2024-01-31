186b0d9605dec9d8e24a322aa830a747c6402330
Akash vaishnav <aakashrankawat19@gmail.com>
2024-01-31 23:23:17 +0530

    updates : used recall score, used optuna for hyper parameter tunning

--- src/Credit_Card_Default_Prediction/components/model_trainer.py
+++ src/Credit_Card_Default_Prediction/components/model_trainer.py
@@ -33,8 +33,20 @@ class ModelTrainer:
             )
 
             models={
-            'XGBClassifier':XGBClassifier(colsample_bytree = 0.8,gamma= 5,max_depth= 4,min_child_weight= 5,subsample= 1.0,n_jobs = -1),
-            'RandomForestClassifier':RandomForestClassifier(n_estimators=300,min_samples_leaf=15,max_depth= None,criterion= 'gini',n_jobs=-1)
+            #'XGBClassifier':XGBClassifier(colsample_bytree = 0.8,gamma= 5,max_depth= 4,min_child_weight= 5,subsample= 1.0,n_jobs = -1),
+            'XGBClassifier':XGBClassifier(objective ='binary:logistic', base_score= None, booster= None, callbacks= None, 
+            colsample_bylevel= None, colsample_bynode= None, colsample_bytree= 0.5487455924565161, device= None, 
+            early_stopping_rounds= None, enable_categorical= False, eval_metric= None, feature_types= None, gamma= None
+            , importance_type= None, interaction_constraints= None, learning_rate=0.08581386246280566,max_bin= None,
+             max_cat_threshold = None, max_cat_to_onehot= None, max_delta_step= None, max_depth = 3, max_leaves =  None,
+              min_child_weight =  3, monotone_constraints=None, multi_strategy=None, n_estimators=136,
+               n_jobs=-1, num_parallel_tree=None, random_state=None, reg_alpha= None, reg_lambda=None, sampling_method=None, 
+               scale_pos_weight= None, subsample= 0.715231665064311, tree_method= None, validate_parameters= None, verbosity= None),
+            #'RandomForestClassifier':RandomForestClassifier(n_estimators=300,min_samples_leaf=15,max_depth= None,criterion= 'gini',n_jobs=-1)
+            'RandomForestClassifier':RandomForestClassifier(max_depth=9, max_features=0.29499734960645674,
+                       min_samples_leaf=6, min_samples_split=16,
+                       n_estimators=121, n_jobs=-1)
+            
             }
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------------------------------            
@@ -52,9 +64,9 @@ class ModelTrainer:
             
             best_model = models[best_model_name]
 
-            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print(f'Best Model Found , Model Name : {best_model_name} , recall Score : {best_model_score}')
             print('\n====================================================================================\n')
-            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , recall Score : {best_model_score}')
 
             save_object(
                  file_path=self.model_trainer_config.trained_model_file_path,
--- src/Credit_Card_Default_Prediction/utils/utils.py
+++ src/Credit_Card_Default_Prediction/utils/utils.py
@@ -35,7 +35,7 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
             # Get R2 scores for train and test data
             #train_model_score = r2_score(ytrain,y_train_pred)
-            test_model_score = f1_score(y_test,y_test_pred)
+            test_model_score = recall_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] =  test_model_score
 
