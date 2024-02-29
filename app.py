12cdea97bfea57be34bd66cc4c212adb8233dbe6
Akash vaishnav <aakashrankawat19@gmail.com>
2024-02-29 09:46:06 +0530

    adding Dockerfile, main.yaml file port update

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def predict_datapoint():
         return render_template("result.html",final_result = result)
     
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(host ="0.0.0.0",port=8080)
 #host ="0.0.0.0",port=8080
